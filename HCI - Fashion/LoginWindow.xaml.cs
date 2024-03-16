d9f831925f2ebbf4ee13c9876b6ac3403b30af08
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-03-17 00:10:13 +0100

    Initial Commit with a lot of prepwork done
    I've created the basic skeleton of the project, as well as started designing the front-end user interaction. TODO: finish creating all basic designs and wireframes and then commit again.

---
+++ HCI - Fashion/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI___Fashion
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UsernameTextBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            UsernameTextBoxBorder.BorderBrush = Brushes.Transparent;
+        }
+
+        private void PasswordTextBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            PasswordTextBoxBorder.BorderBrush = Brushes.Transparent;
+        }
+    }
+}
