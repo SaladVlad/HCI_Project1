991431fef92b3bdd2d369216416f9031dda97a95
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-03-18 18:24:27 +0100

    Adding deleting logic
    Added the entire deleting logic to the app, as well as toast notifications for deletion, validation and all that :smile:

--- HCI - Fashion/MainWindow.xaml.cs
+++ HCI - Fashion/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using HCI___Fashion.Classes;
+using HCI___Fashion.Helpers;
+using Notification.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +23,67 @@ namespace HCI___Fashion
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window  //, INotifyPropertyChanged
     {
-        public MainWindow()
+        public bool IsAdmin { get; }
+
+        Helpers.DataIO IO = new Helpers.DataIO();
+
+        ObservableCollection<ItemContainer> _items;
+        private NotificationManager notificationManager;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<ItemContainer> Items
         {
+            get { return _items; }
+            set { _items = value; }
+        }
+        public MainWindow(bool isAdmin)
+        {
+            //initializing UI component and setting up functionality
+            #region UI Initialization
+
             InitializeComponent();
+            DataContext = this;
+
+            notificationManager = new NotificationManager();
+
+            IsAdmin = isAdmin;
+
+            if (IsAdmin)
+            {
+                AddContentButton.Visibility = Visibility.Visible;
+                AddContentButton.IsEnabled = true;
+                DeleteContentButton.Visibility = Visibility.Visible;
+                DeleteContentButton.IsEnabled = true;
+                ModeLabel.Content = "Admin mode";
+            }
+            else
+            {
+                AddContentButton.Visibility = Visibility.Hidden;
+                AddContentButton.IsEnabled = false;
+                DeleteContentButton.Visibility = Visibility.Hidden;
+                DeleteContentButton.IsEnabled = false;
+                ModeLabel.Visibility = Visibility.Hidden;
+            }
+
+            #endregion
+
+            //Content fetch from xml
+            #region Content fetch
+            try
+            {
+                Items = IO.DeSerializeObject<ObservableCollection<ItemContainer>>("items.xml");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            #endregion
+
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -32,8 +93,93 @@ namespace HCI___Fashion
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
-            //todo messagebox to confirm exiting and saving changes
+            if (IsAdmin)
+            {
+                MessageBoxResult result = MessageBox.Show(
+                               "Save changes before exiting?",
+                               "Confirmation",
+                               MessageBoxButton.YesNoCancel,
+                               MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    Helpers.DataIO io = new Helpers.DataIO();
+                    io.SerializeObject(Items, "items.xml");
+                }
+                else if (result == MessageBoxResult.No)
+                {
+                    //Dont save anything and exit the program
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.Show();
+                    this.Close();
+                }
+
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to exit?",
+                                                    "Confirmation",
+                                                    MessageBoxButton.YesNo,
+                                                    MessageBoxImage.Information);
+                if (result == MessageBoxResult.Yes)
+                {
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.Show();
+                    this.Close();
+                }
+            }
+
+        }
+
+        private void AddContentButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteContentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete the selected items?",
+                               "Warning",
+                               MessageBoxButton.YesNo,
+                               MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+
+                List<ItemContainer> itemsToRemove = new List<ItemContainer>();
+
+                foreach (DataGridColumn column in ContentDataGrid.Columns)
+                {
+                    if (column is DataGridCheckBoxColumn checkBoxColumn)
+                    {
+                        foreach (ItemContainer ic in ContentDataGrid.Items)
+                        {
+                            bool? isChecked = checkBoxColumn.GetCellContent(ic) is CheckBox checkBox ? checkBox.IsChecked : null;
+
+                            if (isChecked == true)
+                            {
+                                itemsToRemove.Add(ic);
+                            }
+                        }
+                    }
+                }
+
+                if (itemsToRemove.Count < 1)
+                {
+                    RaiseToast("noSelectedItems");
+                    return;
+                }
+                foreach (ItemContainer ic in itemsToRemove)
+                {
+                    Items.Remove(ic);
+                }
+            }
+        }
+
+        private void RaiseToast(string semantic)
+        {
+            if (semantic.Equals("noSelectedItems"))
+            {
+                notificationManager.Show("Error", "There are no selected items for deletion!", NotificationType.Error, "WindowNotificationArea");
+            }
         }
     }
 }
