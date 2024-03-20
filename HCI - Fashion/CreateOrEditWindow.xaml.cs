13bed505378ed227ccb320393f91f31c5a8578d8
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-03-20 21:12:42 +0100

    Quite a lot of work on one file

--- HCI - Fashion/CreateOrEditWindow.xaml.cs
+++ HCI - Fashion/CreateOrEditWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using Brush = System.Windows.Media.Brush;
 using Brushes = System.Windows.Media.Brushes;
 
@@ -31,35 +33,44 @@ namespace HCI___Fashion
 
         private Helpers.DataIO io;
 
-        private NotificationManager notificationManager = new NotificationManager();
+        private NotificationManager notificationManager;
 
         private List<System.Drawing.Color> colorList = new List<System.Drawing.Color>();
 
-        public CreateOrEditWindow(bool isAdmin)
-        {
-            InitializeComponent();
-            IsAdmin = isAdmin;
-            io = new Helpers.DataIO();
-            InitUI();
+        private ItemContainer item;
 
-        }
 
         public CreateOrEditWindow(ItemContainer itemContainer,bool isAdmin)
         {
             InitializeComponent();
+            DataContext = this;
+
             IsAdmin = isAdmin;
+            notificationManager = new NotificationManager(Application.Current.Dispatcher);
             io = new Helpers.DataIO();
 
             InitUI();
+            if(itemContainer!=null)
+                FillWithData(itemContainer);
 
-            FillWithData(itemContainer);
+            //passing the reference of the modified item
+            item = itemContainer;
+
         }
 
         private void FillWithData(ItemContainer itemContainer)
         {
             IDTextBox.Text = itemContainer.Id.ToString();
             NameTextBox.Text = itemContainer.Name;
-            ImageFrame.Source = new BitmapImage(new Uri(itemContainer.ImagePath, UriKind.RelativeOrAbsolute));
+            if (itemContainer.ImagePath != null)
+            {
+                ImageFrame.Source = new BitmapImage(new Uri(itemContainer.ImagePath, UriKind.Absolute));
+
+            }
+            else
+            {
+                ImageFrame.Source = new BitmapImage(new Uri("ImgSourceUI/template.jpg", UriKind.RelativeOrAbsolute));
+            }
             io.LoadRtfFile(itemContainer.TextPath, EditorRichTextBox);
             
         }
@@ -81,6 +92,8 @@ namespace HCI___Fashion
                 SaveButton.IsEnabled = false;
 
                 WordCountPanel.Visibility = Visibility.Hidden;
+
+                CancelButton.Content = "Exit";
             }
             else
             {
@@ -109,29 +122,75 @@ namespace HCI___Fashion
             this.DragMove();
         }
 
-        private void BrowseButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+
+            bool allGood = true;
+            try
+            {
+                if (!IDTextBox.Text.Equals(string.Empty))
+                {
+                    int _ = int.Parse(IDTextBox.Text);
+                }
+                else
+                {
+                    RaiseToast("emptyID");
+                }
+
+            }
+            catch
+            {
+                RaiseToast("badID");
+            }
+
+            if (NameTextBox.Text.Equals(""))
+            {
+                RaiseToast("emptyName");
+            }
+
+            if(ImageURLTextBox.Equals("") && 
+                ImageFrame.Source == new BitmapImage(
+                    new Uri("ImgSourceUI/template.jpg", UriKind.RelativeOrAbsolute)))
+            {
+                RaiseToast("imageMissing");
+            }
+
+            if (allGood)
+            {
+
+                item.Id = int.Parse(IDTextBox.Text);
+                item.Name = NameTextBox.Text;
+
+                item.ImagePath = ImageFrame.Source.ToString();
+
+                item.TextPath = "item" + IDTextBox.Text + ".rtf";
+
+                item.CreationDate = DateTime.Now;
+
+                io.SaveAsRtfFile(item.TextPath, EditorRichTextBox);
+
+
+                this.Close();
+            }
 
         }
 
         private void FontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            if (FontFamilyComboBox.SelectedItem != null && !EditorRichTextBox.Selection.IsEmpty)
+            {
+                EditorRichTextBox.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, FontFamilyComboBox.SelectedItem);
+            }
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult res = MessageBox.Show("Do you want to abandon these changes?","Warning",MessageBoxButton.YesNo,MessageBoxImage.Warning);
-            if(res == MessageBoxResult.Yes) 
-                this.Close();
-            else
+            MessageBoxResult res = MessageBoxResult.Yes;
+            //MessageBoxResult res = MessageBox.Show("Do you want to abandon these changes?","Warning",MessageBoxButton.YesNo,MessageBoxImage.Warning);
+            if (res == MessageBoxResult.Yes)
             {
-                //do nothing
+                this.Close();
             }
         }
 
@@ -151,6 +210,17 @@ namespace HCI___Fashion
                 ImageURLTextBox.Text = "Paste new URL...";
                 ImageURLTextBox.Foreground = Brushes.Gray;
             }
+            else
+            {
+                try
+                {
+                    ImageFrame.Source = new BitmapImage(new Uri(ImageURLTextBox.Text,UriKind.Absolute));
+                }
+                catch
+                {
+                    RaiseToast("wrongURL");
+                }
+            }
         }
 
         private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -160,6 +230,16 @@ namespace HCI___Fashion
             string brushName = s.Substring(s.IndexOf('[')+1,s.IndexOf(']')-s.IndexOf('[')-1);
             Brush brush = (Brush)new BrushConverter().ConvertFromString(brushName);
             ColorRectangle.Fill = brush;
+
+            TextRange selectionRange = new TextRange(EditorRichTextBox.Selection.Start, EditorRichTextBox.Selection.End);
+
+            if (selectionRange != null)
+            {
+                selectionRange.ApplyPropertyValue(TextElement.ForegroundProperty, brush);
+            }
+
+
+
         }
 
         private void ImageURLTextBox_KeyDown(object sender, KeyEventArgs e)
@@ -168,9 +248,9 @@ namespace HCI___Fashion
             {
                 try
                 {
-                    ImageFrame.Source = new BitmapImage(new Uri(ImageURLTextBox.Text));
+                    ImageFrame.Source = new BitmapImage(new Uri(ImageURLTextBox.Text,UriKind.Absolute));
                 }
-                catch (Exception ex)
+                catch
                 {
                     RaiseToast("wrongURL");
                 }
@@ -181,17 +261,127 @@ namespace HCI___Fashion
         {
             if (semantic.Equals("wrongURL"))
             {
-                notificationManager.Show("URL is not valid!",NotificationType.Error,"Error");
+                notificationManager.Show("URL is not valid!",NotificationType.Error, "CreateOrUpdateWindowNotificationArea");
+            }
+            if (semantic.Equals("emptyID"))
+            {
+                notificationManager.Show("ID can't be Empty!", NotificationType.Error, "CreateOrUpdateWindowNotificationArea");
+            }
+            if (semantic.Equals("badID"))
+            {
+                notificationManager.Show("ID is NOT a number!", NotificationType.Error, "CreateOrUpdateWindowNotificationArea");
+            }
+            if (semantic.Equals("emptyName"))
+            {
+                notificationManager.Show("Name can't be Empty!", NotificationType.Error, "CreateOrUpdateWindowNotificationArea");
             }
         }
 
         public int CountWords(RichTextBox richTextBox)
         {
             string text = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
-            // Remove any non-word characters (e.g., punctuation, whitespace) and count the remaining words
+
             MatchCollection matches = Regex.Matches(text, @"[\w']+");
             return matches.Count;
         }
 
-}
+        private void EditorRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //update word count
+            WordCountLabel.Content = "Word count: " + CountWords(EditorRichTextBox).ToString();
+        }
+
+        private void EditorRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            //update the UI elements
+            object fontWeight = EditorRichTextBox.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            BoldToggleButton.IsChecked = (fontWeight != DependencyProperty.UnsetValue) && (fontWeight.Equals(FontWeights.Bold));
+
+            object fontStyle = EditorRichTextBox.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            ItalicToggleButton.IsChecked = (fontStyle != DependencyProperty.UnsetValue) && (fontStyle.Equals(FontStyles.Italic));
+
+            object textDecoration = EditorRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            UnderlineToggleButton.IsChecked = (textDecoration != DependencyProperty.UnsetValue) && (textDecoration.Equals(TextDecorations.Underline));
+
+            object fontFamily = EditorRichTextBox.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            FontFamilyComboBox.SelectedItem = fontFamily;
+
+            object fontSize = EditorRichTextBox.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            FontSizeTextBox.Text = fontSize.ToString();
+
+
+            object fontColor = EditorRichTextBox.Selection.GetPropertyValue(Inline.ForegroundProperty);
+
+            if (fontColor is SolidColorBrush solidColorBrush)
+            {
+                System.Windows.Media.Color color = solidColorBrush.Color;
+                string colorName = GetColorName(color);
+                ColorComboBox.SelectedItem = colorName;
+            }
+
+
+        }
+        private string GetColorName(System.Windows.Media.Color color)
+        {
+            foreach (var property in typeof(System.Windows.Media.Colors).GetProperties())
+            {
+                if ((System.Windows.Media.Color)property.GetValue(null) == color)
+                {
+                    return property.Name;
+                }
+            }
+            return color.ToString();
+        }
+
+        private void FontSizeTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //if text size is varying, don't change it because it will mess up the text
+            if (FontSizeTextBox.Text.Contains("{D"))
+            {
+                return;
+            }
+            try
+            {
+                int size = int.Parse(FontSizeTextBox.Text);
+                ChangeFontSize(size);
+            }
+            catch
+            {
+                FontSizeTextBox.Text = "12";
+                ChangeFontSize(12);
+            }
+        }
+
+        private void ChangeFontSize(double size)
+        {
+           
+            TextRange selectionRange = new TextRange(EditorRichTextBox.Selection.Start, EditorRichTextBox.Selection.End);
+
+            if (selectionRange != null)
+            {
+                selectionRange.ApplyPropertyValue(TextElement.FontSizeProperty, size);
+            }
+        }
+
+        private void FontSizeTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("pressed key: " + e.Key);
+            if (e.Key == Key.Enter)
+            {
+
+                try
+                {
+                    double size = int.Parse(FontSizeTextBox.Text);
+                    ChangeFontSize(size);
+                    FontSizeTextBox.Text = size.ToString();
+                }
+                catch
+                {
+
+                    ChangeFontSize(12.0);
+                    FontSizeTextBox.Text = "12";
+                }
+            }
+        }
+    }
 }
